f0496ce7902334bc186e272f4693a8aa8701a24f
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-11 15:22:34 -0700

    updates solution

--- day6.js
+++ day6.js
@@ -1,27 +1,20 @@
 function processData(input) {
     //Enter your code here
-    var arr = input.split("\n")
-    var s1 = arr[1].split('');
-    var s2 = arr[2].split('');
-    var longest;
-    s1.length > s2.length ? longest = s1 : longest = s2;
+    var arr = input.split("\n");
+    let even;
+    let odd;
+    arr.forEach((word) => {
+        if (!isNaN(word)) return // next if word is number;
 
-    var s1Even = []
-    var s2Even = []
-
-    var s1Odd = [];
-    var s2Odd = [];
-
-    for(var i = 0; i <= longest.length -1; i++) {
-        if (i % 2 === 0 || i === 0) {
-            s1Even.push(s1[i])
-            s2Even.push(s2[i])
-        } else {
-            s1Odd.push(s1[i]);
-            s2Odd.push(s2[i]);
-        }
-    }
-    console.log(s1Even.join('') +' '+s1Odd.join(''))
-    console.log(s2Even.join('') +' '+s2Odd.join(''))
-
+        even = '';
+        odd = '';
+        Array.from(word).forEach((char, index) => {
+            if (index === 0 || index % 2 === 0) {
+                even+=char
+            } else {
+                odd+=char
+            }
+        })
+        console.log(even+' '+odd);
+    })
 }
