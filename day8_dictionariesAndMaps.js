789d5a9e33ece29c36721d8caabfdb55029d1a86
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-13 12:34:56 -0700

    updates solution to remove .shift()

--- day8_dictionariesAndMaps.js
+++ day8_dictionariesAndMaps.js
@@ -1,19 +1,17 @@
 function processData(input) {
-    //Enter your code here
-    var args = input.split('\n') // on new line;
+    var args = input.split('\n')
 
-    var n = args.shift() // sets length of arr and removes from args array
-    var arr = []
-    var phoneBook = {}
-    for (var i = 0; i < n; i++) { // loop through from i = 0 to length
-        var nameNum = args.shift() // save name and num and remove from args
-        var arr = nameNum.split(' ') // split string into arr with two vals
-        phoneBook[arr[0]] = arr[1]; // phoneBook[name] = [Num]
+    var n = parseInt(args[0]);
+
+    var phoneBook = {};
+    for(var i = 0; i < n; i++) {
+        var nameNum = args[i+1];
+        nameNum = nameNum.split(' ');
+        phoneBook[nameNum[0]] = nameNum[1];
     }
-
-    args.forEach((name) => {
-        if (phoneBook.hasOwnProperty(name)) {
-            console.log(name+'='+phoneBook[name])
+    for(var i = n+1; i < args.length; i++ ) {
+        if(phoneBook.hasOwnProperty(args[i])) {
+            console.log(args[i]+'='+phoneBook[args[i]])
         } else {console.log('Not found')}
-    })
+    }
 }
