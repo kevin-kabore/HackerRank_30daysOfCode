9be1efdfed64add18035970bd67f87a3b318ccc1
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-07-21 11:49:02 -0700

    completes day 15

---
+++ day15_linkedList.js
@@ -0,0 +1,16 @@
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+function Solution(){
+  this.insert=function(head,data){
+      if (head === null) {
+          return new Node(data)
+      } else if (head.next === null){
+          head.next = new Node(data);
+      } else {
+          this.insert(head.next, data)
+      }
+      return head;
+  };
+}
