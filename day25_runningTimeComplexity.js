3ae9e4b790ef6a5f6d596784c6130f4aa6aa4c09
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-08-01 12:15:20 -0700

    day 25'

---
+++ day25_runningTimeComplexity.js
@@ -0,0 +1,21 @@
+function processData(input) {
+    //Enter your code here
+    input = input.split('\n').map(Number)
+    let n = input[0]
+    for (var i = 1; i <= n; i++){
+      //Brute force
+        isPrime(input[i]) ? console.log('Prime') : console.log('Not prime');
+    }
+    function isPrime(n) {
+        if (n < 2) {
+            return false;
+        } else {
+            for(var i = 2; i < n; i++) {
+                if (n % i === 0) {
+                    return false;
+                }
+            }
+        }
+        return true
+    }
+}
