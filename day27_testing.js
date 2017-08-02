d023bed75a9e482385f14a476569acdb18df0400
Kevin Kabore <kevin.s.kabore@gmail.com>
2017-08-02 16:15:20 -0700

    day27

---
+++ day27_testing.js
@@ -0,0 +1,11 @@
+console.log(5);
+console.log("4 3");
+console.log("-1 0 4 2");
+console.log("6 1");
+console.log("-2 -4 -5 0 3 4");
+console.log("5 3");
+console.log("0 -1 5 6 7");
+console.log("3 2");
+console.log("-9 0 10");
+console.log("7 6");
+console.log("-9 -8 -7 0 9 10 11");
